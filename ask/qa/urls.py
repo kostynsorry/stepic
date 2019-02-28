c0f78e6d117656172294c109fc735f4838738a21
Konstantin Khramov <konstantin_hramo@mail.ru>
2019-02-28 22:47:28 +0300

    fix bug qa/urls.py add singup

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -5,12 +5,15 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.test, name='index'),
+    re_path(r'^signup/', views.test, name='signup'),
     re_path(r'^login/', views.test, name='login'),
     re_path(r'^questions/\d+/', views.test, name='questions'),
     re_path(r'^ask/', views.test, name='ask'),
     re_path(r'^popular/', views.test, name='popular'),
     re_path(r'^new/', views.test, name='new')
+
     # path('', views.test, name='index'),
+    # path('signup/', views.test, name='signup')
     # path('login/', views.test, name='login'),
     # path('questions/<int:index>', views.test, name='questions'),
     # path('ask/', views.test, name='ask'),
