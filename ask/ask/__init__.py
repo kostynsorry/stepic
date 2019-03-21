f24ae25a77748afed2b0c59a7407bd8156551ba4
Konstantin Khramov <konstantin_hramo@mail.ru>
2019-03-21 21:25:16 +0300

    Работа с СУБД

---
+++ ask/ask/__init__.py
@@ -0,0 +1,5 @@
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+from django.db import connection
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    object = QuestionManager()
+    title = models.CharField(max_length=220)
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='likes_set')
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
